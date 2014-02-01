d2e1b7baf626d005ef153ba15283efed850ca116
Honza Dvorsky <jan.dvorsky@yahoo.com>
2014-02-01 21:24:09 +0000

    Initial Commit

---
+++ Project/ToolboxDemo/ToolboxDemo/DUIDemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DUIDemoAppDelegate.h
+//  ToolboxDemo
+//
+//  Created by Honza Dvorsky on 01/02/14.
+//  Copyright (c) 2014 Honza Dvorsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUIDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
